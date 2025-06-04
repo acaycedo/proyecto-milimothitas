4b44a1a4e3d77b72dda6877c0e8526a98fd2a539
Andres Felipe Caycedo Jimenez <afcaycedo14@gmail.com>
2025-06-04 11:49:27 -0500

    Update documentacion end point usuarios y validacion de datos

--- proyecto-milimothitas/src/main/java/com/milimothitas/proyecto/proyecto_milimothitas/model/dto/UserRequest.java
+++ proyecto-milimothitas/src/main/java/com/milimothitas/proyecto/proyecto_milimothitas/model/dto/UserRequest.java
@@ -1,13 +1,27 @@
 package com.milimothitas.proyecto.proyecto_milimothitas.model.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import io.swagger.v3.oas.annotations.media.Schema;
+@Schema(description = "Solicitud de la información de los usuarios")
 
 @Data
 //Clase usada para recibir los datos de un usuario
 public class UserRequest {
+    @Schema(description = "Nombre del usuario", example = "Nombre Apellido")
+    @NotBlank(message = "El campo Nombre es obligatiorio")
+    @Size(min=3, message = "El campo Name debe tener minimo 3 caracteres")
     private String name;
+    @Schema(description = "Correo único del usuario", example = "usuario@correo.com")
+    @NotBlank(message = "El campo Email es obligatiorio")
     private String email;
+    @Schema(description = "Contraseña para el ingreso del usuario", example = "admin123")
+    @NotBlank(message = "El campo Contraseña es obligatiorio")
     private String password;
+    @Schema(description = "Rol que identifica si es usuario o administrador", example = "USER")
+    @NotBlank(message = "El campo Role es obligatiorio")
     private String role;
+    @Schema(description = "Estado del usuario, si es activo o inactivo", example = "Active")
     private String state;
 }
--- proyecto-milimothitas/src/main/java/com/milimothitas/proyecto/proyecto_milimothitas/model/dto/UserResponse.java
+++ proyecto-milimothitas/src/main/java/com/milimothitas/proyecto/proyecto_milimothitas/model/dto/UserResponse.java
@@ -1,14 +1,23 @@
 package com.milimothitas.proyecto.proyecto_milimothitas.model.dto;
 //clase usada para enviar los datos de un usuario
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+@Schema(description = "Respuesta de la información de los usuarios")
 @Data
 public class UserResponse {
+    @Schema(description = "Identificador unico del usuario automatico", example = "1")
     private Long userId;
+    @Schema(description = "Nombre del usuario", example = "Nombre Apellido")
     private String name;
+    @Schema(description = "Correo unico del usuario", example = "usuario@correo.com")
     private String email;
+    @Schema(description = "Contraseña para el ingreso del usuario", example = "admin123")
     private String password;
+    @Schema(description = "Role que identifica si es usuario o administrador", example = "ENUM: USER, ADMIN")
     private String role;
+    @Schema(description = "Estado del usuario, si es activo o inactivo, automatico", example = "Por Defecto: Active")
     private String state;
+    
     
 }
