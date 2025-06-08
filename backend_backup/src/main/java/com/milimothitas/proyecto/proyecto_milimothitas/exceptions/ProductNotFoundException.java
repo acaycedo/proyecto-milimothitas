c740ef4467061b51ef50385b31a4d530b0c1b151
acaycedo <90484168+acaycedo@users.noreply.github.com>
2025-06-08 02:31:18 -0500

    Delete backend_backup directory

--- backend_backup/src/main/java/com/milimothitas/proyecto/proyecto_milimothitas/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.milimothitas.proyecto.proyecto_milimothitas.exceptions;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException (String message){
-        super(message);
-    }
-}
